681e230ace976a419afa3caca5d6573a63099212
darryl-ty <98935440+darryl-ty@users.noreply.github.com>
2023-04-22 20:14:49 -0500

    Implement an interesting startup screen per assigned user story.

--- src/main/java/edu/xula/www/Main.java
+++ src/main/java/edu/xula/www/Main.java
@@ -1,7 +1,36 @@
 package edu.xula.www;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to..");
+        StartScreen();
+    }
+
+    public static void StartScreen(){
+
+        int width = 200;
+        int height = 30;
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics graphics = image.getGraphics();
+
+        Graphics2D graphics2D = (Graphics2D) graphics;
+        graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics2D.drawString("DEGREE AUDIT", 15, 24);
+
+        for (int y = 0; y < height; y++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int x = 0; x < width; x++) {
+                stringBuilder.append(image.getRGB(x, y) == -16777216 ? "*" : " ");
+            }
+
+            if (stringBuilder.toString().trim().isEmpty()) {
+                continue;
+            }
+            System.out.println(stringBuilder);
+        }
     }
 }

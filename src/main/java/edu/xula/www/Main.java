d308d57a6f7845b14e6732da02fa5f7450af59de
darryl-ty <98935440+darryl-ty@users.noreply.github.com>
2023-04-25 13:37:08 -0500

    Fix output of StartScreen() to be more readable.

--- src/main/java/edu/xula/www/Main.java
+++ src/main/java/edu/xula/www/Main.java
@@ -121,7 +121,7 @@ public class Main {
 
         Graphics2D graphics2D = (Graphics2D) graphics;
         graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics2D.drawString("DEGREE AUDIT", 15, 24);
+        graphics2D.drawString("D E G R E E  A U D I T", 15, 24);
 
         for (int y = 0; y < height; y++) {
             StringBuilder stringBuilder = new StringBuilder();
